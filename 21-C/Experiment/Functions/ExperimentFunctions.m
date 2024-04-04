df47f53e148ce4ab62e9d55b55f8b59fac31a785
Leo Moon <haha978@naver.com>
2024-04-04 15:19:52 -0700

    fixed bug for including T1_wait_time for the wait up in the high-field

--- 21-C/Experiment/Functions/ExperimentFunctions.m
+++ 21-C/Experiment/Functions/ExperimentFunctions.m
@@ -1349,7 +1349,7 @@ classdef ExperimentFunctions < handle
 %                             end
 
                         %pause(390);
-                          total_wait_time = 180+T1_wait_time*1e-3;
+                          total_wait_time = 180+T1_wait_time;
                           fprintf("This is total wait time %.3f \n", total_wait_time);
                           pause(total_wait_time); 
                           %pause(500);
